eae64a0b800d35e39f2dfaad61a8b7ca102d2b7d
Akidesign <autumncherry@outlook.jp>
2024-04-11 16:24:58 +0900

    test8 終了

--- section/section8/section8.js
+++ section/section8/section8.js
@@ -1,13 +1,36 @@
-const firstIndex = null;
-const countArrayLength = null;
+const firstIndex = (array) => array[0];
+const countArrayLength = (array) => {
+  if (!Array.isArray(array)) {
+    return "これは配列ではありません";
+  }
+  if (array.length) {
+    return `この配列は${array.length}個のデータを持っています`;
+  } else {
+    return "この配列は空配列です";
+  }
+};
 
-const extractArray = null;
+const extractArray = (array) => array.slice(1, 3);
 
-const makeMark = null;
+const makeMark = (mark, number) => {
+  let array = [];
+  for (let i = 1; i <= number; i++) {
+    array.push(mark);
+  }
+  return array;
+};
 
-const onlyString = null;
+const onlyString = (values) => {
+  const array = values.filter((value) => typeof value === "string");
+  return array;
+};
 
-const addNumber = null;
+const addNumber = (values) => {
+  const newArray = values.map((value, index) => {
+    return `${index + 1}: ${value}`;
+  });
+  return newArray;
+};
 
 module.exports = {
   firstIndex,
