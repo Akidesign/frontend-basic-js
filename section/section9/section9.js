d75ead8e87bcd60a73750df63a755bbd98a9fd9b
Akidesign <autumncherry@outlook.jp>
2024-04-12 11:56:22 +0900

    test9 修正前

--- section/section9/section9.js
+++ section/section9/section9.js
@@ -1,5 +1,44 @@
-const userIdList = null;
+const userIdList = (jsDatas) => {
+  const returnUserId = [];
 
-const fixData = null;
+  const allUserIds = jsonDatas.map((jsonData) => {
+    return jsonData.UserId;
+  });
+
+  for (UserId of allUserIds) {
+    const getUserId = returnUserId.findIndex((eachReturnUserId) => {
+      return eachReturnUserId === UserId;
+    });
+
+    if (getUserId === -1) {
+      returnUserId.push(userId);
+    }
+  }
+
+  return returnUserId;
+};
+
+const fixData = (jsonDatas) => {
+  const userIds = userIdList(jsonDatas);
+  const array = [];
+
+  for (userId of userIds) {
+    const matchedUserData = jsonDatas.filter(
+      (jsonData) => jsonData.userId === userId
+    );
+    for (eachMatchedUserData of matchedUserData) {
+      delete eachMatchedUserData.userId;
+    }
+    array.push(matchedUserData);
+  }
+
+  const newDatas = userIds.map((id, index) => {
+    return {
+      userId: id,
+      datas: array[index],
+    };
+  });
+  return newDatas;
+};
 
 module.exports = { userIdList, fixData };
